ceae615af3ed0b49e3d4f510bb90ae6205b9f29b
cheiz122 <chegep122@gmail.com>
2022-06-30 01:58:08 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
